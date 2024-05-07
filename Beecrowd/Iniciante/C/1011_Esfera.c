73c9052d06ee16c3d9b7ce7e24f8f132a475b4fe
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-07 18:54:59 -0300

    commenting

--- Beecrowd/Iniciante/C/1011_Esfera.c
+++ Beecrowd/Iniciante/C/1011_Esfera.c
@@ -6,15 +6,21 @@ Considere para pi o valor 3.14159.
 
 #include <stdio.h>
 
+// Definição de uma constante pi no valor de 3.14159
 #define pi 3.14159
 
 int main() {
+    // Declaração de variáveis
     double raio, volume;
     
+    // Leitura do raio
     scanf("%lf", &raio);
     
+    // Cálculo do volume
     volume = (4 / 3.0) * pi * raio * raio * raio;
     
+    // Mostra o volume
     printf("VOLUME = %.3lf\n", volume);
+    
     return 0;
 }
