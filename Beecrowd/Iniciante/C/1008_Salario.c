9303f3135733568f4a697b67eb5031065799b450
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-07 18:45:39 -0300

    commenting

--- Beecrowd/Iniciante/C/1008_Salario.c
+++ Beecrowd/Iniciante/C/1008_Salario.c
@@ -10,15 +10,15 @@ A seguir, mostre o número e o salário do funcionário, com duas casas decimais
 #include <stdio.h>
  
 int main() {
-    int num, qnt_hrs;
-    float val_hr, salario;
+    int num, qnt_hrs; // Declara as variáveis num e qnt_horas do tipo inteiro
+    float val_hr, salario; // Declara as variáveis val_hr e salario do tipo ponto flutuante
     
-    scanf("%d\n%d\n%f\n", &num, &qnt_hrs, &val_hr);
+    scanf("%d\n%d\n%f\n", &num, &qnt_hrs, &val_hr); // Lê as variáveis num, qnt_hrs e val_hr
     
-    salario = qnt_hrs * val_hr;
+    salario = qnt_hrs * val_hr; // A variável salario recebe o produto entre qnt_hrs e val_hr
     
-    printf("NUMBER = %d\n", num);
-    printf("SALARY = U$ %.2f\n", salario);
+    printf("NUMBER = %d\n", num); // Mostra o número do funcionário
+    printf("SALARY = U$ %.2f\n", salario); // Mostra o salário do funcionário
     
     return 0;
 }
