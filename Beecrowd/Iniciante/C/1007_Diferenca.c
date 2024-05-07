cdfc36d4cbadc8b1ba21c691169c63656a7cdf45
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-05-07 18:43:10 -0300

    commenting

--- Beecrowd/Iniciante/C/1007_Diferenca.c
+++ Beecrowd/Iniciante/C/1007_Diferenca.c
@@ -7,13 +7,13 @@ Fórmula: DIFERENCA = (A * B - C * D).
 #include <stdio.h>
  
 int main() {
-    int a, b, c, d, diferenca;
+    int a, b, c, d, diferenca; // Declara as variáveis a, b, c, d e diferenca do tipo inteiro
     
-    scanf("%d\n%d\n%d\n%d\n", &a, &b, &c, &d);
+    scanf("%d\n%d\n%d\n%d\n", &a, &b, &c, &d); // Lê as variáveis a, b, c e d
     
-    diferenca = ((a*b) - (c*d));
+    diferenca = ((a*b) - (c*d)); // A variável diferença recebe a subtração dos produtos de a com b e c com d
     
-    printf("DIFERENCA = %d\n", diferenca);
+    printf("DIFERENCA = %d\n", diferenca); // Mostra a diferença
     
     return 0;
 }
