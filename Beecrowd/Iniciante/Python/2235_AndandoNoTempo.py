023d3051ead011b6ee176d4290d972d1f55fab06
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-05 20:09:53 -0300

    walking through time

---
+++ Beecrowd/Iniciante/Python/2235_AndandoNoTempo.py
@@ -0,0 +1,20 @@
+'''
+Imagine que você tenha uma máquina do tempo que pode ser usada no máximo três vezes, e a cada uso da máquina você pode escolher voltar para o passado ou ir para o futuro. 
+A máquina possui três créditos fixos.
+Cada crédito representa uma certa quantidade de anos, e pode ser usado para ir essa quantidade de anos para o passado ou para o futuro. 
+Você pode fazer uma, duas ou três viagens, e cada um desses três créditos pode ser usado uma vez apenas. 
+Por exemplo, se os créditos forem 5, 12 e 9, você poderia decidir fazer duas viagens: ir 5 anos para o futuro e, depois, voltar 9 anos para o passado. 
+Dessa forma, você terminaria quatro anos no passado, em 2012. Também poderia fazer três viagens, todas indo para o futuro, usando os créditos em qualquer ordem, terminando em 2042.
+Neste problema, dados os valores dos três créditos da máquina, seu programa deve dizer se é ou não possível viajar no tempo e voltar para o presente, fazendo pelo menos uma viagem e, no máximo, três viagens.
+Sempre usando cada um dos três créditos no máximo uma vez.
+'''
+
+a, b, c = map(int, input().split()) # Leitura dos valores dos 3 créditos
+
+# Verifica se é possível voltar ao presente com combinações dos créditos
+if a == b + c or b == a + c or c == a + b or \
+   a == b or a == c or b == c or \
+   a + b + c == 0:
+    print("S")
+else:
+    print("N")
