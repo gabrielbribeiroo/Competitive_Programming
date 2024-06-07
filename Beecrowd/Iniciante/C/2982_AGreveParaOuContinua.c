3a197907583f3bbb9bfe994918462a7a2ee16293
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-07 12:17:02 -0300

    line break

--- Beecrowd/Iniciante/C/2982_AGreveParaOuContinua.c
+++ Beecrowd/Iniciante/C/2982_AGreveParaOuContinua.c
@@ -1,5 +1,7 @@
 /*
-A fim de parar a greve geral dos estudantes, o governo realizou uma reunião com a UFSC. Durante a reunião a UFSC expos todos os gastos necessários para manter o funcionamento até o final do ano e o Governo informou valores que poderia oferecer para cobrir esses gastos. A reunião não foi muito organizada, e vários valores individuais foram mencionados, de forma que ninguém sabe se os valores oferecidos são suficientes para cobrir todos os gastos da universidade.
+A fim de parar a greve geral dos estudantes, o governo realizou uma reunião com a UFSC.
+Durante a reunião a UFSC expos todos os gastos necessários para manter o funcionamento até o final do ano e o Governo informou valores que poderia oferecer para cobrir esses gastos. 
+A reunião não foi muito organizada, e vários valores individuais foram mencionados, de forma que ninguém sabe se os valores oferecidos são suficientes para cobrir todos os gastos da universidade.
 Dada a lista de valores citados na reunião, sua tarefa será somas os gastos e as verbas oferecidas para informar os estudantes da UFSC se a greve deve parar ou não.
 */
 
