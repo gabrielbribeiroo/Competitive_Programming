f80041d251841d12d3301a1313e39eb5ebf93d40
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-09 12:03:14 -0300

    time zone

---
+++ Beecrowd/Iniciante/Python/2057_FusoHorario.py
@@ -0,0 +1,15 @@
+'''
+Paulo e Pedro fizeram uma longa jornada desde que partiram do Brasil para competir na Final Mundial da Maratona, em Phuket, Tailândia. 
+Notaram que a cada escala que faziam, tinham que ajustar seus relógios por causa do fuso horário.
+Assim, para melhor se organizarem para as próximas viagens, eles pediram que você faça um aplicativo para celular que, dada a hora de saída, tempo de viagem e o fuso do destino com relação à origem, você informe a hora de chegada de cada vôo no destino.
+Por exemplo, se eles partiram às 10 horas da manhã para uma viagem de 4 horas rumo a um destino que fica à leste, em um fuso horário com uma hora a mais com relação ao fuso horário do ponto de partida, a hora de chegada terá que ser: 10 horas + 4 horas de viagem + 1 hora de deslocamento pelo fuso, ou seja, chegarão às 15 horas. 
+Note que se a hora calculada for igual a 24, seu programa deverá imprimir 0 (zero).
+'''
+
+saida, tempo, fuso = map(int, input().split()) # Leitura da hora da saída, do tempo de viagem e do fuso horário do destino com relação à origem
+chegada = (saida+tempo+fuso) % 24 # Calcula a hora de chegada
+
+if (chegada < 0):
+    chegada += 24 # Ajusta o valor, caso seja negativo
+
+print(chegada) # Imprime a hora loval prevista no destino
