6e039062330f4a209cb6276aa3b1fef74e4a316e
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-11 21:39:43 -0300

    thousandth iteration

---
+++ Beecrowd/Iniciante/Python/3344_Brute.py
@@ -0,0 +1,12 @@
+'''
+Bruno, interessado em aprender inglês, decidiu se matricular no BRUTE (Brazilian University of Theorical English) e logo no primeiro semestre se deparou com a seguinte função na aula de MDI (Matemática do Inglês):
+Seja a função f: N → N, que recebe x e retorna a quantidade de caracteres do valor escrito por extenso.
+Por exemplo:
+f(5) = len(FIVE) = 4
+f(100) = len(ONE HUNDRED) = 11
+A professora de MDI, Kaqui, propôs o seguinte exercício para Bruno:
+Dado um inteiro x, o resultado da primeira iteração na função f, utilizando x, é f(x), da segunda iteração f(f(x)), da terceira é f(f(f(x))) e assim por diante. 
+Qual o resultado da 1000ª iteração?
+'''
+
+print("4")
