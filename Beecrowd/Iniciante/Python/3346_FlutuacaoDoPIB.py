2433cdbf51977eea5526bd057d85699239dd8d95
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-11 21:02:06 -0300

    pib fluctuation calculation

---
+++ Beecrowd/Iniciante/Python/3346_FlutuacaoDoPIB.py
@@ -0,0 +1,18 @@
+'''
+O Sul é uma das regiões que mais contribuem para o Produto Interno Bruto (PIB) do Brasil. 
+No entanto, devido à pandemia de COVID-19, a economia nos três estados do Sul foi muito afetada. 
+Alice, uma pesquisadora da Universidade do Sul, coletou dados sobre a flutuação do PIB de toda a região Sul em cada um dos dois últimos anos. 
+Cada flutuação é expressa por uma porcentagem, de modo que uma porcentagem positiva indica crescimento naquele período de um ano, enquanto que uma porcentagem negativa indica decrescimento.
+Bob, um político, dará uma entrevista à imprensa amanhã. 
+Com base nos dois valores coletados por Alice, Bob deseja calcular a flutuação do PIB correspondente ao período todo dos dois anos analisados, para não falar besteira na entrevista.
+'''
+
+f1, f2 = map(float, input().split()) # Leitura dos valores de entrada
+
+# Cálculo do fator de crescimento para cada ano
+fator_ano1 = 1 + f1 / 100
+fator_ano2 = 1 + f2 / 100
+
+fator_total = fator_ano1 * fator_ano2 # Cálculo do fator de crescimento total ao longo dos dois anos
+flutuacao_total = (fator_total - 1) * 100 # Cálculo da flutuação total em porcentagem
+print(f'{flutuacao_total:.6f}') 
