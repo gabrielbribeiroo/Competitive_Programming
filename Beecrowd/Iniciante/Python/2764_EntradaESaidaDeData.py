20869d1c5fae2c8c0a48f89ac8264db4ac0ed4fe
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-06-15 12:21:54 -0300

    commenting

--- Beecrowd/Iniciante/Python/2764_EntradaESaidaDeData.py
+++ Beecrowd/Iniciante/Python/2764_EntradaESaidaDeData.py
@@ -1,3 +1,11 @@
+'''
+O seu professor gostaria de fazer um programa com as seguintes características:
+Leia uma data no formato DD/MM/AA;
+Imprima a data no formato MM/DD/AA;
+Imprima a data no formato AA/MM/DD;
+Imprima a data no formato DD-MM-AA.
+'''
+
 def main():
     import sys
     input = sys.stdin.read().strip()
