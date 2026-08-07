aa819aa53fcfdc5b45e31217430900a0ebc6a215
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2026-08-07 11:06:05 -0300

    fix(opi2024): reescreve solucao de Competicao.cpp com envoltoria inferior estrita no plano (1/s, 1/r)

--- Studies/OPI2024/Competicao.cpp
+++ Studies/OPI2024/Competicao.cpp
@@ -9,31 +9,23 @@
 using namespace std;
 
 typedef long long ll;
-typedef unsigned long long llu;
-
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
 // OPI 2024 - Problema F: Competição (Codeforces 535E - Tavas and Pashmaks)
 // Encontra todos os competidores que podem vencer para algum valor de S > 0 e R > 0.
-// Utiliza envoltória convexa (Convex Hull / Lower Envelope) com aritmética exata de 128-bit.
+// Utiliza envoltória convexa (Convex Hull / Lower Envelope) no plano (1/s, 1/r).
 
 struct Competitor {
     ll s, r;
     int id;
 };
 
-// Retorna true se P2 é redundante entre P1 e P3
+// Retorna true se P2 é redundante entre P1 e P3 na envoltória inferior
 bool is_redundant(const Competitor &P1, const Competitor &P2, const Competitor &P3) {
-    // P2 é redundante se o ponto de intersecção x23 > x12 (sem intervalo de vitória)
-    // x12 = (P2.s - P1.s) * P1.r * P2.r / ((P1.r - P2.r) * P1.s * P2.s)
-    // x23 = (P3.s - P2.s) * P2.r * P3.r / ((P2.r - P3.r) * P2.s * P3.s)
-    // x23 > x12 <=> (P1.r - P2.r) * (P3.s - P2.s) * P1.s * P3.r > (P2.r - P3.r) * (P2.s - P1.s) * P1.r * P3.s
-
-    unsigned long long lhs = (unsigned long long)(P1.r - P2.r) * (P3.s - P2.s) * P1.s * P3.r;
-    unsigned long long rhs = (unsigned long long)(P2.r - P3.r) * (P2.s - P1.s) * P1.r * P3.s;
-
-    return lhs > rhs;
+    // P1, P2, P3 estão ordenados com s1 > s2 > s3 e r1 < r2 < r3
+    // Todos os termos de diferença são estritamente positivos (<= 10^4), produto máximo 10^16 < ULLONG_MAX
+    unsigned long long lhs = (unsigned long long)(P2.r - P1.r) * (P2.s - P3.s) * P1.s * P3.r;
+    unsigned long long rhs = (unsigned long long)(P1.s - P2.s) * (P3.r - P2.r) * P1.r * P3.s;
+    return lhs <= rhs;
 }
 
 int main() {
@@ -47,15 +39,15 @@ int main() {
         vec[i].id = i + 1;
     }
 
-    // Ordena por s crescente, r decrescente
+    // Ordena por s decrescente, r crescente
     sort(all(vec), [](const Competitor &a, const Competitor &b) {
-        if (a.s != b.s) return a.s < b.s;
-        return a.r > b.r;
+        if (a.s != b.s) return a.s > b.s;
+        return a.r < b.r;
     });
 
-    // Filtra duplicados mantendo o primeiro de cada par (s, r) igual
-    vector<Competitor> unique_comps;
+    // Mapeia IDs para cada par único (s, r)
     map<pair<ll, ll>, vector<int>> same_pair;
+    vector<Competitor> unique_comps;
 
     for (int i = 0; i < n; i++) {
         same_pair[{vec[i].s, vec[i].r}].pb(vec[i].id);
@@ -64,18 +56,18 @@ int main() {
         }
     }
 
-    // Elimina competidores dominados (s_j >= s_i e r_j >= r_i)
+    // Filtra competidores dominados (s_j >= s_i e r_j >= r_i)
+    // Como s é decrescente, mantemos apenas r estritamente crescente
     vector<Competitor> non_dominated;
     ll max_r = 0;
-    for (int i = (int)unique_comps.size() - 1; i >= 0; i--) {
-        if (unique_comps[i].r > max_r) {
-            non_dominated.pb(unique_comps[i]);
-            max_r = unique_comps[i].r;
+    for (const auto &p : unique_comps) {
+        if (p.r > max_r) {
+            non_dominated.pb(p);
+            max_r = p.r;
         }
     }
-    reverse(all(non_dominated));
 
-    // Constrói a Envoltória Convexa (Monotone Stack)
+    // Constrói a envoltória inferior (Monotone Stack)
     vector<Competitor> hull;
     for (const auto &p : non_dominated) {
         while (hull.size() >= 2 && is_redundant(hull[hull.size() - 2], hull.back(), p)) {
@@ -84,7 +76,7 @@ int main() {
         hull.pb(p);
     }
 
-    // Coleta todos os IDs dos competidores vencedores
+    // Coleta IDs de todos os vencedores
     set<int> winner_ids;
     for (const auto &p : hull) {
         for (int id : same_pair[{p.s, p.r}]) {
